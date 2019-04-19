39171616f70e1f2b38b3b085457f2b995f689da6
Justin Kilimnik <justin.kilimnik@gmail.com>
2019-04-19 13:46:36 +1000

    Initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthServiceService } from './auth-service/auth-service.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthServiceService],
 })
 export class AppModule {}
